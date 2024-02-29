fa38632b94d99b5cbe00019c4f5a4d5638e7609e
Jay <jaybodiongan3@gmail.com>
2024-02-29 21:05:49 +0800

    Login/scan UI

--- resources/views/LoginPage.blade.php
+++ resources/views/LoginPage.blade.php
@@ -17,7 +17,7 @@
     @vite(['resources/css/app.css','resources/js/app.js'])
 </head>
 <body>
-    <div class="flex items-center justify-center min-h-screen bg-center bg-no-repeat bg-gray-300 bg-blend-multiply" style="background-image: url('{{ asset('image/login.png') }}'); background-size: cover;">
+    <div class="flex items-center justify-center min-h-screen bg-center bg-no-repeat bg-gray-200 bg-blend-multiply" style="background-image: url('{{ asset('image/login.png') }}'); background-size: cover;">
         <div class="w-full max-w-md p-6 glass-container rounded-md shadow-md ">
             <div class="flex justify-center mb-5">
                 <img src="{{ asset('image/Logo.PNG') }}" alt="Logo" >
@@ -31,7 +31,7 @@
                     <input type="password" placeholder="Password" id="password" name="password" class="mt-1 block w-full sm:w-96 p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500">
                 </div>
                 <div class="flex items-center justify-center">
-                    <button type="submit" class="item px-6 py-2 text-white font-semibold rounded-lg" style="background-color: #6C1B1B;">Login</button>
+                    <button type="submit" class="item px-6 py-2 text-white font-semibold rounded-lg " style="background-color: #6C1B1B;">Login</button>
                 </div>
             </form>
         </div>
--- resources/views/scanner.blade.php
+++ resources/views/scanner.blade.php
@@ -12,21 +12,55 @@
 </head>
 <body>
     <div class="container mx-auto px-4">
-        <div class="flex justify-center items-center min-h-screen">
-            <div class="w-full max-w-xs">
+        <div class="flex justify-center  min-h-screen top-0">
+            <div class="w-full max-w-lx">
                 <video id="preview" class="w-full h-auto"></video>
             </div>
             <div class="ml-10">
-                <label for="default-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">SCAN QR CODE</label>
-                <input type="text" id="default-input" class="bg-gray-50 border rounded-md">
+                <label for="default-input" class="block mb-2 text-xl font-inter text-center  dark:text-white">SCAN QR CODE</label>
+                <input type="text" id="text" readonly value="" class="bg-gray-50 justify-center border rounded-lg w-full">
+                <table class="min-w-full divide-y divide-gray-200 mt-4">
+                    <thead class="bg-gray-50">
+                        <tr>
+                            <th class="px-6 py-3 text-left text-xs font-bold text-gray-900 uppercase tracking-wider">
+                                Date
+                            </th>
+                            <th class="px-6 py-3 text-left text-xs font-bold text-gray-900 uppercase tracking-wider">
+                                Name
+                            </th>
+                            <th class="px-6 py-3 text-left text-xs font-bold text-gray-900 uppercase tracking-wider">
+                                IN
+                            </th>
+                            <th class="px-6 py-3 text-left text-xs font-bold text-gray-900 uppercase tracking-wider">
+                                OUT
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody class="bg-white divide-y divide-gray-200">
+                        <tr>
+                            <td class="px-6 py-4 whitespace-nowrap">
+                                2024/01/12
+                            </td>
+                            <td class="px-6 py-4 whitespace-nowrap">
+                                JAY E. BODIONGAN
+                            </td>
+                            <td class="px-6 py-4 whitespace-nowrap">
+                                7:30AM
+                            </td>
+                            <td class="px-6 py-4 whitespace-nowrap">
+                                9:30AM
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
+        
     </div>
     <script>
         let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
-        scanner.addListener('scan', function (content) {
-            console.log(content);
-            // Here you can handle the scanned content, e.g., display it or send it to a server
+        scanner.addListener('scan',function(c){
+            document.getElementById('text').value=c;
         });
         Instascan.Camera.getCameras().then(function (cameras) {
             if (cameras.length >  0) {
